9f5dde20fbf7623767813209ec4d4d1766233519
cmdragon <2952277346@qq.com>
2025-12-30 14:17:21 +0800

    remove(Document): 移除无效的广告脚本引用
    - 删除了指向 external domain 的无效脚本引用
    - 清理了不再需要的广告集成代码
    - 提高了页面加载性能和安全性

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -20,9 +20,6 @@ export default function Document({ children, locale, bodyClassName }: Props) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2874982874195135"
           crossOrigin="anonymous"
         />
-        <script
-          src="//pl28089020.effectivegatecpm.com/26/db/bd/26dbbd836cc579a60dacafd883ed7349.js"
-        />
       </head>
       <body className={bodyClassName}>{children}</body>
     </html>
